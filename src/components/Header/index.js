c3d06f30265823392651a8aad919efcd63165b44
Rodfc773 <rodrigo.fc773@gmail.com>
2024-05-31 14:09:05 -0300

    Adding the logout function in the system

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserCircle } from 'react-icons/fa';
+import { FaArrowRightFromBracket, FaIdCard } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Nav } from './styled';
+import * as actions from '../../store/modules/auth/action';
+import history from '../../services/history';
 
 export default function Header() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispacth = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    dispacth(actions.loginFailure());
+    history.push('/');
+  };
   return (
     <Nav>
       <Link to={'/'}>
         <FaHome size={24} />
       </Link>
-      <Link to={'/register'}>
-        <FaSignInAlt size={24} />
-      </Link>
-      <Link to="/login">
-        <FaUserAlt size={24} />
-      </Link>
+      {isLoggedIn ? (
+        <Link to={'/register'}>
+          <FaUserCircle size={24} />
+        </Link>
+      ) : (
+        <Link to={'/register'}>
+          <FaIdCard size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn ? (
+        <Link onClick={handleClick} to="">
+          <FaArrowRightFromBracket size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
     </Nav>
   );
 }

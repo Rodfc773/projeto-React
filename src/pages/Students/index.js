f245ff20f2df7ed920ad6bb20c9aca4915868fc8
Rodfc773 <rodrigo.fc773@gmail.com>
2024-06-06 21:59:28 -0300

    Adding the delete's funcionality for the students in the main page and implementing a delete function for the touser's authorization token when the logout is executed

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { get } from 'lodash';
+import { get, noConflict } from 'lodash';
 import { Link } from 'react-router-dom';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation,
+} from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 import { Title, ProfilePicture } from './styled';
 import { Container } from '../../styles/GlobalStyle';
@@ -24,12 +30,41 @@ export default function Students() {
 
     getData();
   }, []);
+
+  const handleFirstStepDelete = (e) => {
+    e.preventDefault();
+
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handeSecondStepDelete = async (e, id, index) => {
+    e.persist();
+    try {
+      setIsLoading(true);
+      await axios.delete(`/students/${id}`);
+
+      const newStudents = [students];
+      newStudents.splice(index, 1);
+      setStudent(newStudents);
+      setIsLoading(false);
+    } catch (error) {
+      const status = get(error, 'response.status', []);
+
+      if (status === 401) {
+        toast.error('You need to be logged for delete a student');
+      } else {
+        toast.error('A error occured while delete a student');
+      }
+    }
+  };
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <Title>Students</Title>
       <StudentsContainer>
-        {students.map((student) => (
+        {students.map((student, index) => (
           <div key={String(student.id)}>
             <ProfilePicture>
               {get(student, 'Files[0].url', false) ? (
@@ -44,9 +79,19 @@ export default function Students() {
             <Link to={`/student/`}>
               <FaEdit size={16} />
             </Link>
-            <Link to={`/student/`}>
+            <Link
+              onClick={handleFirstStepDelete}
+              to={`/student/${student.id}/delete`}
+            >
               <FaWindowClose size={16} />
             </Link>
+
+            <FaExclamation
+              onClick={(e) => handeSecondStepDelete(e, student.id, index)}
+              size={16}
+              display="none"
+              cursor="pointer"
+            />
           </div>
         ))}
       </StudentsContainer>
